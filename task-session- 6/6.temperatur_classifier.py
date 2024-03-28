6f945abb3b771905833c7fe64f9bb16dd709f88a
gelarahadian <gelarahadian38@gmail.com>
2024-03-28 15:11:55 +0700

    remove equal in temperatur

--- task-session- 6/6.temperatur_classifier.py
+++ task-session- 6/6.temperatur_classifier.py
@@ -2,13 +2,13 @@
 def classify_temperature(temp):
     if temp <= 0:
         return("Freezing")
-    elif 0 <= temp <= 10:
+    elif 0 < temp <= 10:
         return("Very Cold")
-    elif 10 <= temp <= 20:
+    elif 10 < temp <= 20:
         return("Cold")
-    elif 20 <= temp <= 30:
+    elif 20 < temp <= 30:
         return("Moderate")
-    elif 30 <= temp <= 40:
+    elif 30 < temp <= 40:
         return("Hot")
     else:
         return("Very Hot")
